1a80432f9f4b45ad8662a4ff1090c71659977570
RoobScoobs <iruben229@gmail.com>
2016-08-01 14:11:36 -0400

    Add resources to albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :examples, except: [:new, :edit]
+  resources :albums, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
