1db572c144e8a7382775a3d5261a0a75f4f47e17
RoobScoobs <iruben229@gmail.com>
2016-08-01 14:13:27 -0400

    Generate Album controller
    Only added index, create, and private actions

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,43 @@
+class AlbumsController < OpenReadController
+  # before_action :set_album, only: [:update, :destroy]
+  before_action :find_album, only: [:show, :update, :destroy]
+
+
+  def index
+    @albums = Album.all.order("created_at DESC")
+
+    render json: @albums
+  end
+
+  def show
+    # render json: Album.find(params[:id])
+  end
+
+  end
+
+  def create
+    @album = current_user.albums.build(album_params)
+
+    if @album.save
+      render json: @album, status: :created, location: @album
+    else
+      render json: @album.errors, status: :unprocessable_entity
+    end
+  end
+
+  # def set_album
+  #   @album = current_user.albums.find(params[:id])
+  # end
+
+  def find_album
+    @album = Album.find(params[:id])
+    # render json: @album
+  end
+
+  def album_params
+    params.require(:album).permit(:title, :description, :artist)
+  end
+
+  private :find_album, :album_params
+
+end
